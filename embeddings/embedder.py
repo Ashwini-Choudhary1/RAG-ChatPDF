1cf9952c0384591b15380b7cc38708375750f827
Ashwini-Choudhary1 <120114731+Ashwini-Choudhary1@users.noreply.github.com>
2026-01-28 01:34:01 +0100

    Added alternative embedding model in embedder file specifically for researh papers

--- embeddings/embedder.py
+++ embeddings/embedder.py
@@ -3,8 +3,8 @@ from typing import List
 
 
 class Embedder:
-    def __init__(self, model_name: str = "all-MiniLM-L6-v2"):
-        
+    def __init__(self, model_name: str = "all-MiniLM-L6-v2"): #alternative 1 : allenai/scibert_scivocab_uncased
+                                                              #alternative 2: sentence-transformers/scibert-base-nli
         self.model = SentenceTransformer(model_name)
 
     def embed_texts(self, texts: List[str]):
